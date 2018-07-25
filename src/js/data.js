21466125c1ff159be9347d154f49105c2ff84aa8
camila <camiramirezpp@gmail.com>
2018-07-24 20:24:00 -0400

    tabla armada con nombres y porcentajes

--- src/js/data.js
+++ src/js/data.js
@@ -1,111 +1,103 @@
-/* Listado de postulantes */
-
 window.onload = () => {
-  dataJSON();
+  database();
 };
 
-function dataJSON() {
-  const btnPre = document.getElementById('btnPreadmission');
+let users = {};
+let progress = {};
+let cohorts = {};
+let usersStats = {};
 
-  const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-  const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-
-  /* Llamar archivos de users JSON*/
-  fetch(usersJSON)
-    .then(response => response.json())
-    .then(dataUser => {
-      console.log(dataUser);
-      renderUsers(dataUser);
-    });
-
-  /* Llamar archivos de progress JSON*/
-  fetch(progressJSON)
-    .then(response => response.json())
-    .then(progressUser => {
-      progress = progressUser;
-      console.log(progress);
-      renderProgress();
-      /* renderProgress(progressUser);*/
-    });
-
-
-  const renderUsers = dataUser => {
-    btnPre.addEventListener('click', () => {
-      const render = dataUser.forEach(element => {
-        const contenedorData = document.createElement('div');
-        const contenedorNames = document.createElement('button');
-        const contenedor = document.getElementById('paragraphList');
-        contenedor.appendChild(contenedorData);
-        contenedorData.appendChild(contenedorNames);
-        let listName = document.createTextNode(element.name);
-        contenedorNames.appendChild(listName);
-      });
-      return render;
-    });
-    // Recorrer los id de los users
-    const renderId = dataUser.forEach(element => {
-      let listIds = document.createTextNode(element.id);
-      console.log(listIds);
-      return listIds;
-    });
-  };
-
-  /* Funcion para poder tomar los datos de progress */
-
-  function renderProgress() {
-    console.log(progress);
-    const match = progress[listIds]; /* variable para hacer "match" entre id de users y progress */
-  }
-}
-
-
-/*
-for (let j in proceso) {
-  let progresId == progreso[i];
-  if (user[i].id === progresId[0]) {
-    console.log(user[i].name+' '+ progresId[1].intro.percent);
-    break;
-  }else {
-    console.log('no');
-    break;
-  }
-}
-const btnUser = document.getElementById('btnPreadmission'); 
-const container = document.getElementById('paragraphList');  //este es el div vacio 
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const cohortsJSON = '../data/cohorts.json';
 
+/* Llamar archivos de users JSON*/
 fetch(usersJSON)
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    renderUsers(data);
+  .then(usersJSON => {
+    users = usersJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener usuarios");
+    //console.error indica un mensaje de error, indica una alerta grave
+    console.error("ERROR > " + error.stack); //error.stack muestra donde falló el codigo, imprime donde esta el error
   });
 
-const renderUsers = (data) => {  recorre el objeto y me devuelve el nombre de cada una 
-  btn.addEventListener('click', () => {
-    const render = data.forEach(element => {
-      return container.innerHTML += `<ul><li><a>${element.name}</a></li></ul>`;
-    });
-  };
+const database = () => {
+/* Llamar archivos de progress JSON*/
+fetch(progressJSON)
+  .then(response => response.json())
+  .then(progressJSON => {
+    progress = progressJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el progreso");
+    console.error("ERROR > " + error.stack);
+  });
 
- 
-  /*
-  const btnUser = document.getElementById('btnPreadmission'); 
-  const container = document.getElementById('paragraphList');  //este es el div vacio 
-  const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-  
-  fetch(usersJSON)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      renderUsers(data);
-    });
-  
-  const renderUsers = (data) => {  recorre el objeto y me devuelve el nombre de cada una 
-    btn.addEventListener('click', () => {
-      const render = data.forEach(element => {
-        return container.innerHTML += `<ul><li><a>${element.name}</a></li></ul>`;
-      });
-      return render;
-    });
-  }; */
+  fetch(cohortsJSON)
+  .then(response => response.json())
+  .then(cohortsJSON => {
+    cohorts = cohortsJSON;
+    areWeFinishedYet();//se llama en todas las respuestas por que no sabemos cual llegara primero y asi nos aseguramos si se ejecutan
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el listado de cohorts");
+    console.error("ERROR > " + error.stack);
+  });
+
+  function areWeFinishedYet() { //¿hemos terminado?
+    // se llama desde todas las promesas para que tome los tome en cuenta sin importar cual de ellos se ejecute primero
+    //vemos si users progress y cohorts ya tienen datos en su interior sino no se ejecuta
+    if (users && progress && cohorts) {
+      const cohort = cohorts.find(item => item.id === 'lim-2018-03-pre-core-pw'); //busca el cohort que tiene ese id ya que este es el unico cohort que esta en los json
+      const courses = Object.keys(cohort.coursesIndex);
+      // guardamos el resultado de llamar a la funcion en una variable global    
+      usersStats = window.computeUsersStats(users, progress, courses);//recibe users, progress y el listado de los cursos del cohort
+    }
+  }
+};
+
+//se declara la funcion para darle funcionalidad al boton toggler y que cambie al darle click
+function onToggleSort() {
+  const direction = toggleSort.innerText;
+  if (direction == "Orden") {
+    toggleSort.innerText = "Ordenar de mayor a menor puntaje";
+  } else {
+    toggleSort.innerText = "Ordenar de menor a mayor puntaje";
+  }
+  //llamamos a la funcion de ordenamiento para que que ordene los usuarios
+  const sortedUsers = window.sortUsers(usersStats, "percent", direction);
+  //no se hace el getElementById por que en JS todo lo declarado en el html con un id queda como variable global :O
+  studentContainer.innerHTML = "";
+  for (let student of sortedUsers) {
+    studentContainer.innerHTML += `
+      <tr>
+      <td style="text-align: center;">${student.name}</td>
+      <td>${student.stats.exercises.percent} % </td>
+      <td>${student.stats.quizzes.percent} %</td>
+      <td>${student.stats.reads.percent} %</td>
+      <td>${student.stats.percent} %</td>
+      </tr> 
+    `;
+  }
+}
+
+function onSearchBoxChange() {
+  const search = searchBox.value;
+  const filteredUsers = window.filterUsers(usersStats, search);
+  studentContainer.innerHTML = "";
+  filteredUsers.forEach(student => {
+    studentContainer.innerHTML += `
+    <tr>
+    <td style="text-align: center;">${student.name}</td>
+    <td>${student.stats.exercises.percent} % </td>
+    <td>${student.stats.quizzes.percent} %</td>
+    <td>${student.stats.reads.percent} %</td>
+    <td>${student.stats.percent} %</td>
+    </tr> 
+    `;
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -16,9 +16,150 @@ function enter() {
 const btnPre = document.getElementById('btnPreadmission');
 btnPre.addEventListener('click', () => {
   document.getElementsByClassName('listPreadmission')[0].classList.remove('listPreadmission');
-  document.getElementsByClassName('personalInfo')[0].classList.remove('personalInfo');
+  document.getElementsByClassName('toggleSort')[0].classList.remove('toggleSort');
 });
 
-/* Busqueda con input*/
 
+window.computeUsersStats = (
+  users, 
+  progress, 
+  courses,
+) => {
+  for (let i = 0; i < users.length; i++) {
+    let userId = users[i].id;
+    
+    let userProgress = progress[userId];
+    if (JSON.stringify(userProgress) === '{}') {  
+      users[i].stats = {
+        percent: 0,
+        exercises: { percent: 0, },
+        reads: { percent: 0, },
+        quizzes: {
+          percent: 0,
+          scoreAvg: 0,
+        }
+      };
+    } else {
+      
+      let percentGral = 0;
+      let lectures = 0;
+      let lecturesCompleted = 0;
+      let lecturesPercent = 0;
+      let quizzes = 0;
+      let quizzesCompleted = 0;
+      let quizzesPercent = 0;
+      let exercises = 0;
+      let exercisesCompleted = 0;
+      let exercisesPercent = 0;
+      let scoreSum = 0;
+      let scoreAvg = 0;
 
+      //Recorriendo cada id de curso
+      for (let i in userProgress) { 
+        let element = userProgress[i];
+        if (courses.indexOf(i) < 0) {
+          continue;
+        }
+        //Porcentaje general
+        percentGral += element.percent / Object.keys(userProgress).length;
+        for (let unit of Object.values(element.units)) { 
+          for (let part of Object.values(unit.parts)) {
+            if (part.length === 0) {
+              quizzes = 0;
+              exercises = 0;
+              lectures = 0;
+              quizzesPercent = 0;
+              exercisesPercent = 0;
+              lecturesPercent = 0;
+            }
+            if (part.type === 'read') {
+              lectures++;
+            }
+
+            if (part.type === 'read' && part.completed === 1) {
+              lecturesCompleted++;
+            }
+            
+            lecturesPercent = Math.round((lecturesCompleted * 100) / lectures);
+            
+            if (part.type === 'quiz') { 
+              quizzes++;
+            }
+            if (part.type === 'quiz' && part.completed === 1) {
+              quizzesCompleted++;
+              scoreSum += part.score;
+            }
+            quizzesPercent = Math.round((quizzesCompleted * 100 * 10 / quizzes)) / 10;  
+            if (part.type === 'practice') {
+              exercises++;
+            }
+            if (part.type === 'practice' && part.completed === 1) {
+              exercisesCompleted++;
+            }
+            exercisesPercent = Math.round((exercisesCompleted * 100 * 10) / (exercises || 1)) / 10;
+          } 
+        } 
+      }
+      // saca promedio
+      scoreAvg = scoreSum / quizzes;
+
+      users[i].stats = {
+        percent: percentGral,
+        reads: {
+          percent: lecturesPercent,
+          total: lectures,
+          completed: lecturesCompleted
+        },
+        exercises: {
+          percent: exercisesPercent,
+          total: exercises,
+          completed: exercisesCompleted
+        },
+        quizzes: {
+          percent: quizzesPercent,
+          total: quizzes,
+          completed: quizzesCompleted,
+          scoreAvg: scoreAvg,
+          scoreSum: scoreSum
+        }
+      };
+    }
+  }
+  return users;
+  console.log(users);
+}
+
+//esta funcion ordena por orden alfabetico a las alumnas
+window.sortUsers = (users, orderBy, orderDirection) => {  
+  if (orderBy === "name") { 
+    return users.sort(function (a, b) {
+      if (orderDirection == "ASC") {
+        return a.name.localeCompare(b.name);
+      } else {
+        return a.name.localeCompare(b.name) * -1;
+      }
+    });
+  }
+
+  if(orderBy === "percent"){
+    return users.sort((a,b)=>{
+      if(orderDirection == "ASC"){
+        return a.stats.percent - b.stats.percent;
+      }else{
+        return (a.stats.percent - b.stats.percent)*-1;
+      }
+    });
+  }
+};
+
+window.filterUsers = (users, search) => {
+  if(search){
+    if(users){
+      search =search.toLowerCase();
+      return  users.filter(user => user &&
+         user.name &&
+        user.name.toLowerCase().indexOf(search)>= 0);
+    }
+  }
+  return users;
+};
